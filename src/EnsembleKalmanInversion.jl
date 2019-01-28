dec85e94306b7cddda04a81f65ccbb968f3223d5
Jack Muir <jmuir@caltech.edu>
2019-01-28 08:38:46 -0800

    Attempting to reset name of file

---
+++ src/EnsembleKalmanInversion.jl
@@ -0,0 +1,26 @@
+module EnsembleKalmanInversion
+
+    using LowRankApprox: AbstractLinearOperator, LinearOperator, pheigfact, PartialHermitianEigen
+    import Base: convert, size, transpose
+    using Compat
+
+    if VERSION < v"0.7"
+        import Base: ishermitian, issymmetric
+    else
+        using Random: randperm
+        using Nullables
+        import LinearAlgebra
+        using LinearAlgebra: UniformScaling, I, Diagonal
+        using Statistics: mean
+        using Distributed: addprocs, pmap
+    end
+
+    export eki
+    export heki
+
+    include("covarianceoperator.jl")
+    include("basiceki.jl")
+    include("hierarchicaleki.jl")
+ 
+
+end
